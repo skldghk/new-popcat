ece52ac02c745646cd4b29e4a43fd2e136e318a0
skldghk <as204k@naver.com>
2024-06-06 20:43:42 +0900

    Update project with latest changes and remove unnecessary files

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,11 +1,14 @@
-import mysql from 'mysql2/promise';
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.DB_HOST || 'svc.sel5.cloudtype.app',
+  port: process.env.DB_PORT || 31577,
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'As31882773@@',
+  database: process.env.DB_NAME || 'click_database',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-export default pool;
+module.exports = pool;
